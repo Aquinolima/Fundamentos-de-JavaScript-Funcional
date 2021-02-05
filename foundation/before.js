2657adec41849c17dc27ba05bc2ee3d65c5e87de
Thiago Aquino <aquinolima.thiago@gmail.com>
2021-02-05 17:26:42 -0300

    before starting the code

---
+++ foundation/before.js
@@ -0,0 +1,17 @@
+// 1. Prerequisites
+
+// -> Basic knowledge of JS
+// ---> Data types: number, string, boolean, array and object
+// ---> Operators (+, -, *, /, &&, ||, >=, <=, ...)
+// ---> If, For, While
+
+// 2. Node
+// -> NodeJS (v12.19.0)
+
+// 3.Visual Studio Code
+// -> Code Rinner
+// ---> Code-runeer: Clear Previous Output
+// ---> Ctrl + Alt + n (run code)
+// ---> Ctrl + Alt + m (stop the running code)
+
+console.log('Teste version 2');
