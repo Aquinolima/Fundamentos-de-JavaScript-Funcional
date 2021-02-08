c82e3b261de71f411522ba0aa75faf313123f238
Thiago Aquino <aquinolima.thiago@gmail.com>
2021-02-08 17:51:23 -0300

    Filter Function

---
+++ functional/Filter.js
@@ -0,0 +1,29 @@
+// Filter Function 
+// Essa Function recebe outra Function como parâmetro.
+
+//Usando Function Filter para filtrar um Array em outro Array que retorna somente o true
+
+
+const numbers = [1, 2, 3, 4, 5, 6]
+
+const greaterThanZero = el => el > 0
+const greaterThanTen = el => el > 10
+const even = el => el % 2 === 0
+
+console.log(numbers.filter(el => el > 0))
+console.log(numbers.filter(greaterThanZero))
+console.log(numbers.filter(greaterThanTen))
+console.log(numbers.filter(even))
+
+
+const students = [
+    { name: 'Jake', score: 6.4 },
+    { name: 'Susan', score: 8.6 },
+    { name: 'Emma', score: 9.4 },
+    { name: 'Peter', score: 9.1 }
+]
+
+const greatStudent = students => students.score >= 9
+
+console.log(students.filter(greatStudent));
+console.log(students);
